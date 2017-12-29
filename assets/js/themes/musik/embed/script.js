48e9e84117045f84742149630715d0ffd7c9ee35
cozacomen <34930660+cozacomen@users.noreply.github.com>
2017-12-29 13:11:02 +0900

    Update script.js

--- assets/js/themes/musik/embed/script.js
+++ assets/js/themes/musik/embed/script.js
@@ -55,7 +55,7 @@ function hhmmss(secs)
 
 
   var tag = document.createElement('script');
-  tag.src = "https://www.youtube.com/iframe_api";
+  tag.src = "http://www.youtube.com/iframe_api";
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
   
